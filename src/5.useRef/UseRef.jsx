d767df412c0e0e50180054779299380a5bb7ca12
behzad <behzadaminipour87@gmail.com>
2024-02-18 20:15:09 +0330

    edit useRef

--- src/5.useRef/UseRef.jsx
+++ src/5.useRef/UseRef.jsx
@@ -3,11 +3,9 @@ import { useRef, useState } from "react";
 // we use useRef if we nedd to get access to dom element or
 // we need to store some value as a mutable value between re-rendering our app
 export default function UseRef() {
-  console.log("render");
   const [userName, setUserName] = useState("");
   const inputRef = useRef();
   const onTextFocus = () => {
-    console.log("onTextFocus", inputRef.current.value);
     inputRef.current.focus();
     inputRef.current.value = "Hello";
   };
